9e609884c0fed1a1cb0cfc3ac3ae5da179d1f708
Владимир <fwthedream@mail.ru>
2019-01-27 20:52:28 +0700

    Added a bit more Kata.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +12,7 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            Console.WriteLine(CodeWars.Kata.SpinWords("Hey fellow warriors"));
+            Console.WriteLine(  CodeWars.Kata.compBest(new int[0], null));
         }
     }
 }
